ecf0634fe932d5183fe54f05a74aea842a2fe0ed
giopetterin <gio.petterin@gmail.com>
2018-03-05 21:59:49 -0300

    Commit giovanni
    -Sin detalles

--- src/ar/com/PSGMecanico/modelo/dominio/Persona.java
+++ src/ar/com/PSGMecanico/modelo/dominio/Persona.java
@@ -5,5 +5,26 @@
 package ar.com.PSGMecanico.modelo.dominio;
 
 public class Persona {
+	
+	private String nombre;
 
+	/**
+	 * 
+	 * @param nombre
+	 */
+	public Persona(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	
 }
