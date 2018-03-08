20b6ea8c508e9d22e128eb759221e90ab4a4bf47
mauricioarielramirez <mauricioarielramirez@gmail.com>
2018-03-07 22:08:48 -0300

    Agregado recursos
    - Resources/DataBaseConnector/mysql-connector-java-5.1.45-bin.jar
    - src/ar/com/PSGMecanico/accesodatos/persistencia/HibernateUtil.java
    - src/ar/com/PSGMecanico/accesodatos/persistencia/hibernate.cfg.xml

---
+++ src/ar/com/PSGMecanico/accesodatos/persistencia/HibernateUtil.java
@@ -0,0 +1,53 @@
+package ar.com.PSGMecanico.accesodatos.persistencia;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.context.internal.ThreadLocalSessionContext;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+public class HibernateUtil {
+	
+    private static SessionFactory sessionFactory;
+    
+    public static synchronized void buildSessionFactory() {
+        if (sessionFactory == null) {
+            Configuration configuration = new Configuration();
+            configuration.configure("/ar/com/santalucia/accesodatos/persistencia/hibernate.cfg.xml");
+            configuration.setProperty("hibernate.current_session_context_class", "thread");
+           ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+           sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+       }
+   }
+
+   public static void openSessionAndBindToThread() {
+       Session session = sessionFactory.openSession();
+       ThreadLocalSessionContext.bind(session);
+   }
+
+
+   public static SessionFactory getSessionFactory() {
+       if (sessionFactory==null)  {
+           buildSessionFactory();
+       }
+       return sessionFactory;
+   }
+
+   public static void closeSessionAndUnbindFromThread() {
+       Session session = ThreadLocalSessionContext.unbind(sessionFactory);
+       if (session!=null) {
+           session.close();
+       }
+   }
+
+   public static void closeSessionFactory() {
+       if ((sessionFactory!=null) && (sessionFactory.isClosed()==false)) {
+           sessionFactory.close();
+       }
+   }	
+
+
+}
